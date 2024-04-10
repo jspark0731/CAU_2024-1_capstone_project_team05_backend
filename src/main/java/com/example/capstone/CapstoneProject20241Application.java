b6d41b45b0d1d76d58bde90fee58c7975295d26c
jspark0731 <parkjunseok0731@gmail.com>
2024-04-10 10:22:38 +0900

    Modified the typo in the project name.

--- src/main/java/com/example/capstone/CapstoneProject20241Application.java
+++ src/main/java/com/example/capstone/CapstoneProject20241Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.example.capstone.mapper")
-public class CapstroneProject20241Application
+public class CapstoneProject20241Application
 {
 	public static void main(String[] args)
 	{
-		SpringApplication.run(CapstroneProject20241Application.class, args);
+		SpringApplication.run(CapstoneProject20241Application.class, args);
 	}
 }
