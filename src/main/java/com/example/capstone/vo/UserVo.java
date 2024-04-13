4cdbcb6bf2c502881026b16e8e46bad29c22a77c
jspark0731 <parkjunseok0731@gmail.com>
2024-04-14 01:08:12 +0900

    Modified the DB setting(Shrinking Schema) for experiments

--- src/main/java/com/example/capstone/vo/UserVo.java
+++ src/main/java/com/example/capstone/vo/UserVo.java
@@ -9,8 +9,4 @@ public class UserVo {
         private String username;
         private String email;
         private String password;
-        private String address;
-        private String phone;
-        private String website;
-        private String company;
 }
